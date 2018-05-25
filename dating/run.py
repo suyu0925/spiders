37c3f3ca0e151e91f639d100f0ecfd7f9a836361
lckof-mac <suyu0925@gmail.com>
2018-05-25 18:00:34 +0800

    婚介爬虫：修正只爬取到了最后一页的bug，补充年龄和城市

--- dating/run.py
+++ dating/run.py
@@ -8,8 +8,8 @@ html_file = './data/index.html'
 
 
 def main():
+  girls = []
   for page in range(1, 11):
-    girls = []
     # if not os.path.exists(html_file):
     if True:
       get = requests.get(base_url + '/Recommend/index.html?p={}'.format(page))
@@ -27,13 +27,19 @@ def main():
       name = li.select('.searchm2right > .searchm2info > a')[0].get_text()
       gender = li.select('.searchm2right > .searchm2info')[
           1].get_text().replace('性别：', '')
+      age = int(li.select('.searchm2right > .searchm2info')[
+          2].get_text().replace('年龄：', '').replace('岁', ''))
+      city = li.select('.searchm2right > .searchm2info')[
+          3].get_text().replace('城市：', '')
 
       # 只要女性
       if gender == '女':
         girls.append({
             'avatar': base_url + avatar_url,
             'name': name,
-            'gender': gender
+            'gender': gender,
+            'age': age,
+            'city': city
         })
 
   # save girls
