f66019c51c52abc96d7492ee6db2a68bd87b4982
suyu <13936562@qq.com>
2020-12-22 14:44:04 +0800

    支持直接传入番号当参数

--- jav/115.py
+++ jav/115.py
@@ -1,6 +1,7 @@
 import sqlite3
 import sys
 from loguru import logger
+import re
 
 if __name__ == '__main__':
     logger.info(f'argv: {sys.argv}')
@@ -11,17 +12,27 @@ if __name__ == '__main__':
 
     conn = sqlite3.connect('jav.db')
     c = conn.cursor()
-    c.execute(f"SELECT alias FROM actors WHERE alias LIKE '%{actor_arg}%'")
-    actor = c.fetchone()
-    if actor is None:
-        logger.warning(f'there is not actor {actor_arg}')
-    logger.info(f'actor: {actor}')
 
-    actor_clause = ' OR '.join([f"actor LIKE '%{x}%'" for x in actor[0].split(',')])
-    date_clause = f'AND date > {date_arg}' if date_arg else ''
-    sql_query = f"SELECT uid FROM films WHERE ({actor_clause}) {date_clause} ORDER BY date DESC"
-    c.execute(sql_query)
-    films = c.fetchall()
+    if len(sys.argv) == 2 and re.search(r'\w+-\d+', actor_arg):
+        # 番号
+        uid_arg = actor_arg
+        logger.info(f'番号: {uid_arg}')
+        sql_query = f"SELECT uid FROM films WHERE uid LIKE '%{uid_arg}%'"
+        c.execute(sql_query)
+        films = c.fetchall()
+    else:
+        c.execute(f"SELECT alias FROM actors WHERE alias LIKE '%{actor_arg}%'")
+        actor = c.fetchone()
+        if actor is None:
+            logger.warning(f'there is not actor {actor_arg}')
+        logger.info(f'actor: {actor}')
+
+        actor_clause = ' OR '.join(
+            [f"actor LIKE '%{x}%'" for x in actor[0].split(',')])
+        date_clause = f'AND date > {date_arg}' if date_arg else ''
+        sql_query = f"SELECT uid FROM films WHERE ({actor_clause}) {date_clause} ORDER BY date DESC"
+        c.execute(sql_query)
+        films = c.fetchall()
     logger.info(f'films: {films}')
 
     magnets = []
@@ -34,15 +45,17 @@ if __name__ == '__main__':
         ]
         magnet = None
         for tags in tags_priority:
-            tag_clause = ' '.join([f"AND tag like '%{x}%'" for x in tags.split(',')])
-            c.execute(f"SELECT href FROM magnets WHERE uid = '{film[0]}' {tag_clause}")
+            tag_clause = ' '.join(
+                [f"AND tag like '%{x}%'" for x in tags.split(',')])
+            c.execute(
+                f"SELECT href FROM magnets WHERE uid = '{film[0]}' {tag_clause}")
             magnet = c.fetchone()
             if magnet:
                 magnets.append(magnet[0])
                 break
         if magnet is None:
             logger.info(f'not found magnet for {film[0]}')
-    
+
     with open('output.txt', 'w', encoding="utf-8") as f:
         for magnet in magnets:
             f.write(magnet + '\n')
--- jav/README.md
+++ jav/README.md
@@ -28,5 +28,10 @@ scrapy crawl magnet -a actor='波多野結衣'
 
 输出磁力链接到文件，比如2020-04-19之后逢見リカ的影片：
 ```bash
-python3 115.py 逢見リカ 2020-04-19
+python 115.py 逢見リカ 2020-04-19
+```
+
+也可以直接输出具体某个番号
+```bash
+python 115.py snis-919
 ```
