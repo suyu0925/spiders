1afbc69653ce59d4c6e8ef0ab08d816c7bf82d86
suyu <13936562@qq.com>
2020-12-22 14:18:36 +0800

    质量高的片源优先

--- jav/115.py
+++ jav/115.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         logger.error('usage: python3 115.py 逢見リカ [2020-04-19]')
     actor_arg = sys.argv[1]
-    date_arg = sys.argv[2]
+    date_arg = sys.argv[2] if len(sys.argv) > 2 else None
 
     conn = sqlite3.connect('jav.db')
     c = conn.cursor()
@@ -17,17 +17,31 @@ if __name__ == '__main__':
         logger.warning(f'there is not actor {actor_arg}')
     logger.info(f'actor: {actor}')
 
-    c.execute(f"SELECT uid FROM films WHERE actor LIKE '%{actor[0]}%' AND date > {date_arg}")
+    data_clause = f'AND date > {date_arg}' if date_arg else ''
+    c.execute(f"SELECT uid FROM films WHERE actor LIKE '%{actor[0]}%' {data_clause} ORDER BY date DESC")
     films = c.fetchall()
     logger.info(f'films: {films}')
 
     magnets = []
     for film in films:
-        c.execute(f"SELECT href FROM magnets WHERE uid = '{film[0]}'")
-        magnet = c.fetchone()
-        if not magnet is None:
-            magnets.append(magnet[0])
+        tags_priority = [
+            '高清,字幕,優',
+            '高清,字幕',
+            '高清',
+            ''
+        ]
+        magnet = None
+        for tags in tags_priority:
+            tag_clause = ' '.join([f"AND tag like '%{x}%'" for x in tags.split(',')])
+            c.execute(f"SELECT href FROM magnets WHERE uid = '{film[0]}' {tag_clause}")
+            magnet = c.fetchone()
+            if magnet:
+                magnets.append(magnet[0])
+                break
+        if magnet is None:
+            logger.info(f'not found magnet for {film[0]}')
     
-    with open('output.txt', 'w') as f:
+    with open('output.txt', 'w', encoding="utf-8") as f:
         for magnet in magnets:
             f.write(magnet + '\n')
+        logger.info(f"output {actor_arg} {date_arg if date_arg else ''} done")
